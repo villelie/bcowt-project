4db1cd406cca3413b13fbefb3f9834f43d39842f
amanuela97 <44023180+amanuela97@users.noreply.github.com>
2019-11-30 16:02:39 +0200

    changes to project-app.js to fix login issue.

--- project-app.js
+++ project-app.js
@@ -7,13 +7,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const app = express();
-<<<<<<< HEAD
 const httpPort = 3033;
-=======
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 
 app.use(express.urlencoded({extended: true}));
 
@@ -41,66 +35,28 @@ if (process.env.SERVER === 'server') {
 	console.log('Using secure cookie');
 } else console.log('Using unsecure cookie');
 
-<<<<<<< HEAD
-=======
-app.use(require('express-session')(sesOptions));
-
-passport.use(new localStrategy((username, password, done) => {
-	if(username !== 'villa') {
-		console.log('wrong user');
-		return done(null, false);
-	}
-	return done(null, {username: username});
-}));
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 
 passport.serializeUser((user, done) => {
 	done(null, user.username);
 });
 
-<<<<<<< HEAD
 passport.deserializeUser((user, done) => {
 	done(null, {username: user});
-=======
-passport.deserializeUser((username, done) => {
-	done(err, {username: username});
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 if (process.env.SERVER === 'local') {
 	require('./secure/localhost')(app);
 } else {
 	require('./secure/server')(app);
-	app.listen(3033, () => {
+	app.listen(httpPort, () => {
 		console.log('Started');
 	});
 }
 
-<<<<<<< HEAD
 app.use(express.static('public'));
-
-app.get('/usergetall', async (req, res) => {
-=======
-app.post('/userlogin', passport.authenticate('local', {failureRedirect: '/fail'}), (req, res) => {
-	console.log('trying to login');
-	res.redirect('/');
-});
-
-app.post('/useradd', async (req, res) => {
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
-	try {
-		const salt = bcrypt.genSaltSync(12);
-		const hash = bcrypt.hashSync(req.body.register_userpass, salt);
-		res.json(await dbUsers.insert(req.body.register_username, req.body.register_useremail, hash));
-	} catch (e) {
-		console.log(e);
-		res.send('db error :(');
-	}
-});
 
 app.get('/usergetall', async (req, res) => {
 	try {
@@ -112,30 +68,32 @@ app.get('/usergetall', async (req, res) => {
 });
 
 app.get('/usersearch', async (req, res) => {
+	console.log(req.query);
 	try {
-<<<<<<< HEAD
+		res.json(await dbUsers.search(req.query.search_username));
+	} catch(e) {
+		res.send('db error :(');
+	}
+});
+
+app.post('/useradd', async (req, res) => {
+	console.log(req.body);
+	try {
 		const salt = bcrypt.genSaltSync(12);
 		const hash = bcrypt.hashSync(req.body.register_userpass, salt);
 		await res.json(await dbUsers.insert(req.body.register_username, req.body.register_useremail, hash));
 	} catch (e) {
 		console.log(e);
-=======
-		res.json(await dbUsers.search(req.query.search_username));
-	} catch(e) {
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 		res.send('db error :(');
 	}
 });
 
-<<<<<<< HEAD
 app.post('/userlogin', passport.authenticate('local', {failureRedirect:'/fail'}),
     (req, res) => {
 	console.log('trying to login');
 	res.redirect('/');
 });
 
-=======
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 app.get('/', (req, res) => {
 	console.log('is user in req', req.user);
 	res.sendFile('./public/main.html', {root: __dirname});
@@ -145,10 +103,6 @@ app.get('/fail', (req, res) => {
 	res.send('failed to login');
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 16f452c912cc37b76ea1a923ae4594b30cb312f9
 app.get('/register', (req, res) => {
 	console.log('is user in req', req.user);
 	res.sendFile('./public/register.html', {root: __dirname});
