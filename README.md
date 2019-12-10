df1f686bfa3d2d6816cf87df91be34b1f3f0872a
villelie <ville.liekari@metropolia.fi>
2019-12-10 14:49:16 +0200

    readme and docs

--- README.md
+++ README.md
@@ -5,13 +5,21 @@
 npm i
 node project-app.js
 ```
+### Generate cert
+```
+yum install mod_ssl openssl
+openssl genrsa -out ca.key 2048
+openssl req -new -key ca.key -out ca.csr
+openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
+
+```
 ### head to https://localhost:8088/ in your browser
 ### .env example
 ```
 DB_HOST=localhost
 DB_USER=asd
 DB_PASS=asd
-DB_NAME=bcowtproject
+DB_NAME=bcowt_project
 SERVER=local
 KEY=./ca.key
 CERT=./ca.crt
---
+++ bcowt_project.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 10, 2019 at 01:32 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bcowt_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pics`
+--
+
+CREATE TABLE `pics` (
+  `pic_id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL DEFAULT 0,
+  `pic_title` varchar(40) NOT NULL,
+  `pic_desc` text NOT NULL,
+  `pic_file` text NOT NULL,
+  `pic_likes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(16) NOT NULL,
+  `user_email` text NOT NULL,
+  `user_pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pics`
+--
+ALTER TABLE `pics`
+  ADD PRIMARY KEY (`pic_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pics`
+--
+ALTER TABLE `pics`
+  MODIFY `pic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
