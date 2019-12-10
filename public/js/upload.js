0bff8513004c112eacdc387bdcc0dc1eb41e14f6
Daniel Finnerman <danu95d@hotmail.com>
2019-12-10 12:54:48 +0200

    er-model, comments

--- public/js/upload.js
+++ public/js/upload.js
@@ -7,16 +7,22 @@ const createPicCards = (pics) => {
 	ul.innerHTML = '';
 	pics.forEach((pic) => {
 		// create li with DOM methods
+		//pic ownder to pic card
 		const p2 = document.createElement('p');
 		p2.innerText = pic.pic_title + ' - ' + pic.pic_desc + ' - by: ' + pic.owner;
+		
 		const div4 = document.createElement('div');
 		div4.classList = 'container';
 		div4.appendChild(p2);
+		
+		//pic likes to pic card
 		const p1 = document.createElement('p');
 		const likes = pic.pic_likes;
 		p1.innerText = likes;
 		const div3 = document.createElement('div');
 		div3.className = 'likecontainer';
+		
+		//button like click to database
 		const but = document.createElement('button');
 		but.type = 'button';
 		but.alt = pic.pic_id;
@@ -29,6 +35,8 @@ const createPicCards = (pics) => {
 		});
 		div3.appendChild(but)
 		div3.appendChild(p1);
+		
+		//rescaled thumbnail of the photo to its place
 		const img = document.createElement('img');
 		img.src = url + '/thumbnails/' + pic.pic_file;
 		img.alt = pic.pic_title;
